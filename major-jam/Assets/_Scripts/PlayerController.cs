835b9cdd17bac31cfe28dd81043e06c1ae8903b7
Jakub Starzec <jakub.starzec@rqgames.net>
2021-08-13 22:10:53 +0200

    Collision with objects

--- major-jam/Assets/_Scripts/PlayerController.cs
+++ major-jam/Assets/_Scripts/PlayerController.cs
@@ -1,14 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
+    Scene currentScene;
     public float moveSpeed = 5f;
     public Transform movePoint;
+
+    public enum JanosikForms{Walk, Ghost, Hover, Slime}
+    public JanosikForms currentForm;
+    public LayerMask wall;
+    public LayerMask hole;
     void Start()
     {
         movePoint.parent = null;
+        currentForm = JanosikForms.Walk;
+        currentScene = SceneManager.GetActiveScene();
     }
 
     void Update()
@@ -17,15 +26,41 @@ public class PlayerController : MonoBehaviour
 
         if(Vector3.Distance(transform.position, movePoint.position) <= .05f)
         {
-            if(Mathf.Abs(Input.GetAxisRaw("Horizontal")) == 1f)
+            
+            if(Mathf.Abs(Input.GetAxisRaw("Horizontal")) == 1f && Mathf.Abs(Input.GetAxisRaw("Vertical")) == 0f)
             {
-                movePoint.position += new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f);
+                if(!Physics2D.OverlapCircle(movePoint.position + new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f), .1f, wall))
+                {
+                    movePoint.position += new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f);
+                }
             }
 
-             if(Mathf.Abs(Input.GetAxisRaw("Vertical")) == 1f)
+            if(Mathf.Abs(Input.GetAxisRaw("Vertical")) == 1f && Mathf.Abs(Input.GetAxisRaw("Horizontal")) == 0f)
             {
-                movePoint.position += new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f);
+                if(!Physics2D.OverlapCircle(movePoint.position + new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f), .1f, wall))
+                {
+                    movePoint.position += new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f);
+                }
             }
+
+
         }
     }
+
+    private void OnTriggerEnter2D(Collider2D other) 
+    {
+        switch(currentForm)
+        {
+            case JanosikForms.Walk:
+                if(other.tag == "Fire" || other.tag == "Hole")
+                {
+                    SceneManager.LoadScene(currentScene.name);
+                }
+                break;
+            default:
+                Debug.Log("No form");
+                break;
+        }
+    }
+    
 }
