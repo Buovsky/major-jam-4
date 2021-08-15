97f4db5cd3df8f34a31241d7a4f00cddd3bbc219
pilotpirxie <10637666+pilotpirxie@users.noreply.github.com>
2021-08-15 19:24:30 +0200

    Add tutorial

---
+++ major-jam/Assets/_Scripts/TutorialController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UnityEngine;
+
+public class TutorialController : MonoBehaviour
+{
+        private Tutorial _tutorial;
+        public string Text;
+        
+        private void Start()
+        {
+                _tutorial = FindObjectOfType<Tutorial>();
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+                if (other.gameObject.CompareTag("Player"))
+                {
+                        _tutorial.Display(Text);
+                }
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+                if (other.gameObject.CompareTag("Player"))
+                {
+                        _tutorial.Close();
+                }
+        }
+}
