66e254c55e1a2a8418092f87cbd1259a9c9f004b
pilotpirxie <10637666+pilotpirxie@users.noreply.github.com>
2021-08-16 01:04:34 +0200

    Add main menu

---
+++ major-jam/Assets/_Scripts/MenuScript.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour
+{
+    private bool isMenuShown;
+
+    [SerializeField] private GameObject overlay;
+    [SerializeField] private GameObject fadeIn;
+    
+    [SerializeField] private AudioSource audioSource;
+    [SerializeField] private AudioClip menuSound;
+    
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            if (isMenuShown)
+            {
+                fadeIn.SetActive(true);
+                Invoke("LoadGameplay", 4f);
+                audioSource.PlayOneShot(menuSound);
+            }
+            else
+            {
+                isMenuShown = true;
+                overlay.SetActive(isMenuShown);
+                audioSource.PlayOneShot(menuSound);
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            isMenuShown = false;
+            overlay.SetActive(isMenuShown);
+            audioSource.PlayOneShot(menuSound);
+        }
+    }
+
+    void LoadGameplay()
+    {
+        SceneManager.LoadScene("Level2");
+    }
+}
